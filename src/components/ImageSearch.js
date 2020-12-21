a8c7d97e199c2d852d52921d2b8c415620019220
Caio Augusto <caioamfr@gmail.com>
2020-12-21 19:37:49 -0300

    Indented code

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ImageSearch = ({ searchText }) => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     e.preventDefault();
-    searchText(text)
-  }
+    searchText(text);
+  };
   return (
-    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-          <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Procure uma imagem pelo termo..." />
+          <input
+            onChange={e => setText(e.target.value)}
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Procure uma imagem pelo termo..."
+          />
           <button className="buttonSearch" type="submit">
             Search
-    </button>
+          </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ImageSearch
+export default ImageSearch;
